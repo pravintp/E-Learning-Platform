8bbbd658188b3bd189e52cf53857bf6633810a9f
pravintp <pravin@testpress.in>
2021-12-22 17:33:37 +0530

    Add support to render different types of content

--- educa/educa/settings.py
+++ educa/educa/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "students.apps.StudentsConfig",
+    "embed_video",
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,6 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 LOGIN_REDIRECT_URL = reverse_lazy("student_course_list")
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = os.path.join(BASE_DIR, "media/")
